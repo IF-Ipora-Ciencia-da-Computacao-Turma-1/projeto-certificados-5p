cb9e60e71f70a42a1be3f7fc05f2c977ec78e322
Murillo Santos de Castro <murillocastro2009@hotmail.com>
2022-07-04 21:34:43 -0300

    login3

---
+++ js/login.js
@@ -0,0 +1,7 @@
+let esqueceuSenha = document.getElementById("esqueceuSenha");
+
+esqueceuSenha.addEventListener("click",alertaDeSenha());
+
+function alertaDeSenha(){
+    /*alert("vc esta sem senha. \nopss !!!!! ");*/
+}
